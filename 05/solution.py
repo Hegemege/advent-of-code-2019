984ec414fe0728fe5d8a171b31e13df4c0533663
Henri Niva <henri.a.niva@gmail.com>
2019-12-05 20:35:31 +0200

    Day 5 part 2

--- 05/solution.py
+++ 05/solution.py
@@ -5,34 +5,37 @@ def run_program(memory, input_buffer):
     pc = 0
     while True:
         result_code, pc_offset = execute_instruction(memory, pc, input_buffer)
-        if result_code != 0:
+        if result_code == -1:  # Halt instruction
             return
-        pc += pc_offset
+        if result_code == 0:  # Non-jump instructions
+            pc += pc_offset
+        elif result_code == 1:  # Jump instructions
+            pc = pc_offset
 
 
 def execute_instruction(memory, position, input_buffer):
     instruction_header = memory[position]
     op_code = int(str(instruction_header)[-2:])
     if op_code == 99:
-        return (1, 1)
+        return (-1, 1)
 
     # Get parameter modes and pad the rest
     parameter_modes_str = str(instruction_header)[:-2][::-1]
     parameter_modes_str = parameter_modes_str.ljust(MAX_INST_PARAM_COUNT, '0')
     parameter_modes = list(map(int, parameter_modes_str))
 
+    # Add and multiply
     if op_code == 1 or op_code == 2:
         operator = int.__add__ if op_code == 1 else int.__mul__
-        operand1_value = memory[position + 1]
-        operand2_value = memory[position + 2]
-        operand1 = memory[operand1_value] if parameter_modes[0] == 0 else operand1_value
-        operand2 = memory[operand2_value] if parameter_modes[1] == 0 else operand2_value
+        parameter1 = get_parameter(memory, position, 1, parameter_modes)
+        parameter2 = get_parameter(memory, position, 2, parameter_modes)
         write_addr = memory[position + 3]
 
-        memory[write_addr] = operator(operand1, operand2)
+        memory[write_addr] = operator(parameter1, parameter2)
 
         return (0, 4)
 
+    # Input
     if op_code == 3:
         write_addr = memory[position + 1]
         input_value = input_buffer.pop(0)
@@ -40,21 +43,52 @@ def execute_instruction(memory, position, input_buffer):
         memory[write_addr] = input_value
         return (0, 2)
 
+    # Output
     if op_code == 4:
-        operand_value = memory[position + 1]
-        output_value = memory[operand_value] if parameter_modes[0] == 0 else operand_value
+        output_value = get_parameter(memory, position, 1, parameter_modes)
         print("OUT".ljust(6, ' ') + str(output_value))
         return (0, 2)
 
+    # Jump-if-true && jump-if-false
+    if op_code == 5 or op_code == 6:
+        parameter1 = get_parameter(memory, position, 1, parameter_modes)
+        parameter2 = get_parameter(memory, position, 2, parameter_modes)
+
+        # A XNOR B
+        if (parameter1 == 0) == (op_code == 5):
+            return (0, 3)
+        return (1, parameter2)
+
+    # Less-than && equals
+    if op_code == 7 or op_code == 8:
+        operator = int.__lt__ if op_code == 7 else int.__eq__
+        parameter1 = get_parameter(memory, position, 1, parameter_modes)
+        parameter2 = get_parameter(memory, position, 2, parameter_modes)
+        write_addr = memory[position + 3]
+
+        memory[write_addr] = 1 if operator(parameter1, parameter2) else 0
+
+        return (0, 4)
+
+    print("OPCODE NOT IMPLEMENTED:", op_code)
+
+
+def get_parameter(memory, position, offset, parameter_modes):
+    return memory[memory[position + offset]] if parameter_modes[offset - 1] == 0 else memory[position + offset]
+
 
 def part1(part_input):
+    print("PART 1")
     memory = parse_input_file(part_input)
     input_buffer = [1]
     run_program(memory, input_buffer)
 
 
 def part2(part_input):
-    pass
+    print("PART 2")
+    memory = parse_input_file(part_input)
+    input_buffer = [5]
+    run_program(memory, input_buffer)
 
 
 def parse_input_file(input_file_contents):
