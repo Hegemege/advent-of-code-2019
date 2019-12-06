122bf683eba2143ace479e676b4e138a3db11a74
Henri Niva <henri.a.niva@gmail.com>
2019-12-06 15:20:24 +0200

    Day 6 part 2

--- 06/solution.py
+++ 06/solution.py
@@ -10,8 +10,15 @@ class Node:
         for child in self.children:
             child.set_depth(depth + 1)
 
+    def get_ancestors(self, ancestors):
+        if self.parent is None:
+            return ancestors
 
-def part1(part_input):
+        ancestors.append(self.parent)
+        return self.parent.get_ancestors(ancestors)
+
+
+def build_tree(part_input):
     # Create nodes for every object in the data
     object_lookup = {}
     for orbit in part_input:
@@ -31,12 +38,32 @@ def part1(part_input):
     # Calculate depth of the tree. COM starts at 0 depth
     object_lookup["COM"].set_depth(0)
 
+    return object_lookup
+
+
+def part1(part_input):
+    object_lookup = build_tree(part_input)
     # Sum the depths of every node
     print(sum(map(lambda x: x.depth, object_lookup.values())))
 
 
 def part2(part_input):
-    pass
+    object_lookup = build_tree(part_input)
+    # Find the first common ancestor of YOU and SAN
+    # i.e. the common ancestor with largest depth
+    # Sum the steps required to reach that ancestor
+    you_orbit = object_lookup["YOU"].parent
+    san_orbit = object_lookup["SAN"].parent
+
+    you_ancestors = you_orbit.get_ancestors([])
+    san_ancestors = san_orbit.get_ancestors([])
+
+    common_ancestors = set(you_ancestors).intersection(set(san_ancestors))
+
+    first_common_ancestor = max(common_ancestors, key=lambda x: x.depth)
+    # Solution is the distance between the common ancestor and the target nodes summed
+    print(you_orbit.depth - first_common_ancestor.depth +
+          san_orbit.depth - first_common_ancestor.depth)
 
 
 if __name__ == '__main__':
