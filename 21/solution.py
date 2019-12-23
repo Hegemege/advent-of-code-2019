69b72d25bd6219bb3cbf39f6f3795c42f2dabf2d
Henri Niva <henri.a.niva@gmail.com>
2019-12-23 20:21:13 +0200

    Day 21 part 1

---
+++ 21/solution.py
@@ -0,0 +1,58 @@
+from intcode import Intcode
+import math
+
+
+def data_to_ascii(data):
+    return "".join(map(chr, data))
+
+
+def ascii_to_data(data):
+    return list(map(ord, data))
+
+
+def part1(part_input):
+    print("PART1")
+
+    memory = parse_input_file(part_input)
+    input_buffer = []
+    output_buffer = []
+    computer = Intcode(memory, input_buffer, output_buffer)
+
+    # Feed instructions
+    instructions = [
+        "OR A T",  # Check that A, B or C has a hole
+        "AND B T",
+        "AND C T",
+        "NOT T J",  # If any of ABC is a hole, set JUMP
+        "AND D J",  # but only if 4th spot is free
+        #
+        "WALK",
+    ]
+
+    for instruction in instructions:
+        for data_input in ascii_to_data(instruction):
+            computer.set_input(data_input)
+        computer.set_input(ord("\n"))
+
+    computer.run_program()
+
+    if len(computer.output_buffer) > 100:
+        print(data_to_ascii(computer.output_buffer))
+    else:
+        print(data_to_ascii(computer.output_buffer[:-1]))
+        print(computer.output_buffer[-1])
+
+
+def part2(part_input):
+    print("PART2")
+
+
+def parse_input_file(input_file_contents):
+    return list(map(int, input_file_contents.split(",")))
+
+
+if __name__ == "__main__":
+    with open("input", "r") as input_file:
+        input_file_contents = input_file.readline().strip()
+        part1(input_file_contents)
+        part2(input_file_contents)
