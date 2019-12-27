01c2a84fb205069867453f6ba652813a0005fd88
Henri Niva <henri.a.niva@gmail.com>
2019-12-27 20:07:19 +0200

    Day 16 part 2

--- 16/solution.py
+++ 16/solution.py
@@ -50,12 +50,13 @@ def part1(part_input):
         # Convert the signal
         for i in range(len(input_signal)):
             pattern = pattern_lookup[i]
-            output_signal[i] = int(
-                str(
+            output_signal[i] = (
+                abs(
                     sum(
                         [pattern[j] * input_signal[j] for j in range(len(input_signal))]
                     )
-                )[-1:]
+                )
+                % 10
             )
 
         input_signal = output_signal
@@ -63,8 +64,26 @@ def part1(part_input):
     print("".join(map(str, input_signal[:8])))
 
 
-def part2(nodes):
+def part2(part_input):
     print("PART2")
+
+    signal = part_input * 10000
+
+    # offset it past the midpoint, no need to check any values below it
+    # Only additions of digits, fft multipliers are 0's and 1's
+    offset = int("".join([str(n) for n in signal[:7]]))
+
+    for _ in range(100):
+        n_sum = sum(signal[offset:])
+        for n in range(offset, len(signal)):
+            # Current multipliers are   000...01111...1
+            # Next multipliers are      000...00111...1
+            # Add the numbers together and for the next row, subtract current
+            old = signal[n]
+            signal[n] = n_sum % 10
+            n_sum -= old
+
+    print("".join(map(str, part_input[offset : offset + 8])))
 
 
 def parse_input_line(line):
